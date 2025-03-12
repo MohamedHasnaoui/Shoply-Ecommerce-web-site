1b402a6edb2946b7b2988bb7349aaea76ba62fa0
lamrabet-oussama <lamrabetoussama184@gmail.com>
2025-03-12 23:31:14 +0100

    pull last changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,29 @@
-import {Routes,Route}from 'react-router'
-import Login from "./Pages/Common/LoginPage"
-import ClientLayout from "./layout/ClientLayout"
-import Register from "./Pages/Common/RegisterPage"
-import ProtectedRoute from './Components/common/ProtectedRoute'
-import OTPVerification from './Pages/Common/OTPVerificationPage'
-import SellerLayout from './layout/SellerLayout'
-import AddProduct from './Pages/Seller/addProductPage'
+import { Routes, Route } from "react-router";
+import Login from "./Pages/Common/LoginPage";
+import ClientLayout from "./layout/ClientLayout";
+import Register from "./Pages/Common/RegisterPage";
+import ProtectedRoute from "./Components/common/ProtectedRoute";
+import OTPVerification from "./Pages/Common/OTPVerificationPage";
+import SellerLayout from "./layout/SellerLayout";
+import AddProduct from "./Pages/Seller/addProductPage";
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route element={<SellerLayout />} >
-        <Route path='/seller' element={<AddProduct />} />
-      </Route>
-      <Route element={<ClientLayout />}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/verify-email" element={<OTPVerification />} />
-        <Route element={<ProtectedRoute />}>
-        {/* ! protected routes here */}
+      <Routes>
+        <Route element={<SellerLayout />}>
+          <Route path="/seller" element={<AddProduct />} />
         </Route>
-      </Route>
-     </Routes>
+        <Route element={<ClientLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify-email" element={<OTPVerification />} />
+          <Route element={<ProtectedRoute />}>
+            {/* ! protected routes here */}
+          </Route>
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
