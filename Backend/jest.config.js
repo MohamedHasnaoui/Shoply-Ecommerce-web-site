07806d0ea87f8d7a85660d957a8934eb388f3b54
mohamed.elhasnaoui <mohamed.elhasnaoui121@gmail.com>
2025-10-21 19:50:22 +0100

    dixed some test bugs

--- Backend/jest.config.js
+++ Backend/jest.config.js
@@ -5,7 +5,10 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 export default {
   preset: "ts-jest/presets/default-esm",
-  
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/", // 👈 Ignore compiled JS tests
+  ],
   extensionsToTreatAsEsm: [".ts"],
   testEnvironment: "node",
   transform: {
